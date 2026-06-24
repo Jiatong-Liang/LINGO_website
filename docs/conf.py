3c7bce7318d87b60c3571a25f82dd5558ed35999
Jiatong <jkliang@umich.edu>
2026-06-23 21:13:24 -0400

    create website

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ copyright = "2026, Ziyang Liu"
 # If you don’t need the separation provided between version and release,
 # just set them both to the same value.
 try:
-    from LINGO import __version__ as version
+    from lingo import __version__ as version
 except ImportError:
     version = ""
 
